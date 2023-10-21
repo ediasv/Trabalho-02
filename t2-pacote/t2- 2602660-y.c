46146a71e6f230d82acbdb729ac6370983c1018d
FelipeReis64 <76824530+FelipeReis64@users.noreply.github.com>
2023-10-21 15:38:01 -0300

    Add files via upload

---
+++ t2-pacote/t2- 2602660-y.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "trabalho2.h"
+#include "wavfile.h"
+
+void mudaGanho(double *dados, int n_amostras, double ganho) {
+  for (int i = 0; i < n_amostras; i++) {
+    dados[i] = dados[i] * ganho;
+  }
+}
+
+int contaSaturacoes(double *dados, int n_amostras) {
+  int saturacoes = 0;
+  for (int i = 0; i < n_amostras; i++) {
+    if (dados[i] > 1 || dados[i] < -1)
+      saturacoes++;
+  }
+  return saturacoes;
+}
+
+int hardClipping(double *dados, int n_amostras, double limite) {
+  int n_alterados = 0;
+  for (int i = 0; i < n_amostras; i++) {
+    if (dados[i] > limite) {
+      dados[i] = limite;
+      n_alterados++;
+    }
+    else if (dados[i] < -1 * limite) {
+      dados[i] = -1 * limite;
+      n_alterados++;
+    }
+  }
+  return n_alterados;
+}
+
+void limitaSinal(double *dados, int n_amostras, int n_passos);
+
+void geraOndaQuadrada(double *dados, int n_amostras, int taxa, double freq);
