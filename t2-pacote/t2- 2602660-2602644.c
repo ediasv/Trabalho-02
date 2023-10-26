29d707a5386a56b0900ec17638f0d07264baf078
Eduardo Dias <eduardoviniciusdias@alunos.utfpr.edu.br>
2023-10-26 11:02:35 -0300

    update
    Declarando os "int i" fora do for;
    Adicionei comentários.

--- t2-pacote/t2- 2602660-2602644.c
+++ t2-pacote/t2- 2602660-2602644.c
@@ -5,16 +5,25 @@
 #include "trabalho2.h"
 #include "wavfile.h"
 
+void mudaGanho(double *dados, int n_amostras, double ganho); 
+int contaSaturacoes(double *dados, int n_amostras); 
+int hardClipping(double *dados, int n_amostras, double limite); 
+void limitaSinal(double *dados, int n_amostras, int n_passos);
+void geraOndaQuadrada(double *dados, int n_amostras, int taxa, double freq);
+
 void mudaGanho(double *dados, int n_amostras, double ganho) {
   int i;
+  // Para cada dado do vetor, multiplicar ele pelo ganho.
   for (i = 0; i < n_amostras; i++) {
     dados[i] = dados[i] * ganho;
   }
 }
 
 int contaSaturacoes(double *dados, int n_amostras) {
-  int saturacoes = 0;
-  for (int i = 0; i < n_amostras; i++) {
+  int saturacoes, i;
+  saturacoes = 0;
+  // Pada cada dado do vetor, verificar se está além do limite. Em caso afirmativo, incrementa o número de saturações.
+  for (i = 0; i < n_amostras; i++) {
     if (dados[i] > 1 || dados[i] < -1)
       saturacoes++;
   }
@@ -22,12 +31,16 @@ int contaSaturacoes(double *dados, int n_amostras) {
 }
 
 int hardClipping(double *dados, int n_amostras, double limite) {
-  int n_alterados = 0;
-  for (int i = 0; i < n_amostras; i++) {
+  int n_alterados, i;
+  n_alterados = 0;
+  // Para cada dado do vetor...
+  for (i = 0; i < n_amostras; i++) {
+    // Se o dado estiver acima do limite superior, "clipa".
     if (dados[i] > limite) {
       dados[i] = limite;
       n_alterados++;
     }
+    // Se o dado estiver abaixo do limite inferior, "clipa".
     else if (dados[i] < -1 * limite) {
       dados[i] = -1 * limite;
       n_alterados++;
